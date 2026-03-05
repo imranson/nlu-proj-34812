e6832c24516eb13ab499552667229046dfbc88ad
chun.tham@student.manchester.ac.uk <q12628ct@login1.csf3.man.alces.network>
2026-03-05 21:49:37 +0000

    edit conda env name

--- jobscripts/jobscript-1-test.sh
+++ jobscripts/jobscript-1-test.sh
@@ -18,7 +18,7 @@ module purge
 conda deactivate
 
 module load libs/cuda
-conda activate nlu-proj-34812
+conda activate nlu-proj-348
 
 echo "Job is using $SLURM_GPUS GPU(s) with ID(s) $CUDA_VISIBLE_DEVICES and $SLURM_NTASKS CPU core(s)"
 
