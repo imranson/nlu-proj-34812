6616e18062205aba33cb479c9e1488c3176bc55e
I'm Ranson <imranson@Ransons-MacBook-Jesus-Pro.local>
2026-03-24 21:24:23 +0000

    change lr

--- jobscripts/jobscript-5-siamese-margin-150.sh
+++ jobscripts/jobscript-5-siamese-margin-150.sh
@@ -25,7 +25,7 @@ echo "Job is using $SLURM_GPUS GPU(s) with ID(s) $CUDA_VISIBLE_DEVICES and $SLUR
 python3 -c "import torch;
 print(f'torch cuda is available: {torch.cuda.is_available()}')"
 
-python code/train_av-4.py --batch_size 64 --epochs 300 --margin 1.5 --lr 2e-4
+python code/train_av-4.py --batch_size 64 --epochs 300 --margin 1.5 --lr 2e-5
 
 module purge
 conda deactivate
